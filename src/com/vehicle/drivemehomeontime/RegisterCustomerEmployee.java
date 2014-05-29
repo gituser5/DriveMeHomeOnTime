431f3f6ee121e972e9ba6b5b762b39ae1f76fcda
gituser5 <rrrlui@ucdavis.edu>
2014-05-29 02:07:35 -0700

    my first commit

---
+++ src/com/vehicle/drivemehomeontime/RegisterCustomerEmployee.java
@@ -0,0 +1,23 @@
+package com.vehicle.drivemehomeontime;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterCustomerEmployee extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_customer_employee);
+        
+    }
+    
+    public void submitRegistration(View v)
+    {
+        
+    }
+
+}
